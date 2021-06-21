a5486cd05a57533171c6de721c7d2d1083c755c0
Thiago Marafeli <thiagomarafeli@usp.br>
2021-06-21 06:24:23 -0300

    Finalização do jogo

--- src/main/java/Modelo/Main.java
+++ src/main/java/Modelo/Main.java
@@ -2,6 +2,10 @@ package Modelo;
 
 import Controler.Tela;
 
+//Jogo feito por:
+//Pedro Garcia
+//Thiago Marafeli
+
 public class Main {
     public static void main(String[] args) {
         java.awt.EventQueue.invokeLater(new Runnable() {
